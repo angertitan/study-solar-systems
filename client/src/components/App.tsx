7975e7e2769b682081b73e8864c202bf05abc703
Jan Schmidt <me@jnschmdt.de>
2019-06-05 15:05:21 +0200

    set up react

---
+++ client/src/components/App.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+// routing, etc.
+
+const App: React.FunctionComponent = () => {
+  const greeting = 'Hello its me React';
+  return <h1>{greeting}</h1>;
+};
+
+export default App;
